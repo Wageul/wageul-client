46dcab2c8dcfdad117b1bbb7974aa29fc626cbcd
khhan <jacob2572502@gmail.com>
2024-06-25 11:49:20 +0900

    removed leave membership and added link to edit myprofile

--- app/(layout)/user/myprofile/page.tsx
+++ app/(layout)/user/myprofile/page.tsx
@@ -4,21 +4,24 @@ import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
 import { Button } from "@/components/ui/wageulButton";
 import CountingTextArea from "@/components/CountingTextArea";
 import { AboutMe, ProfileHeader, ReviewList } from "@/components/Profile";
+import Link from "next/link";
 
 export default async function Page({ params }: { params: { id: string } }) {
   return (
     <>
       <ProfileHeader />
       <div className="mt-[35px] flex justify-center">
-        <Button variant={"white"} size={"xl"}>
-          Edit profile
-        </Button>
+        <Link href={'/user/myprofile/edit'}>
+          <Button variant={"white"} size={"xl"}>
+            Edit profile
+          </Button>
+        </Link>
       </div>
       <AboutMe />
       <ReviewList />
-      <section className="mt-[35px] flex justify-center">
+      {/* <section className="mt-[35px] flex justify-center">
         <div className="text-primary-red">Leave Membership</div>
-      </section>
+      </section> */}
     </>
   );
 }
