8c97aa6b10aadd27af9c844b3d9d9979b45c448b
khhan <jacob2572502@gmail.com>
2024-06-26 17:11:49 +0900

    implemented carousel

--- app/(layout)/experience/[id]/page.tsx
+++ app/(layout)/experience/[id]/page.tsx
@@ -1,12 +1,71 @@
+"use client";
+
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: string } }) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
     <div className="bg-grey-2">
-      <div className="w-[320px] h-[309px] bg-grey-2">image Carousel</div>
-      <section>
+      {/* <div className="w-[320px] h-[309px] bg-grey-2"> */}
+      <div>
+        <Carousel setApi={setApi} className="relative w-full h-[309px]">
+          <CarouselContent>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="relative flex h-[309px] items-center justify-center p-6 rounded-[20px] overflow-hidden">
+                  <Image
+                    src="/main.avif"
+                    fill={true}
+                    alt="thumbnail"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="absolute mb-[13px] flex justify-center gap-[3px] bottom-0 w-full">
+            {Array.from({ length: count }).map((_, index) => (
+              <div
+                key={index}
+                className={`size-1.5 rounded-full ${
+                  index + 1 === current ? "bg-primary-red" : "bg-grey-4"
+                }`}
+              ></div>
+            ))}
+          </div>
+        </Carousel>
+      </div>
+      <section className="mt-2.5">
         <div className="px-[8px] py-[16px] bg-background rounded-[16px] text-h2 text-center">
           Going to a cart bar like a Korean office worker!
         </div>
